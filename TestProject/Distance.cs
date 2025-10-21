a567d755bc54fd66b4d7f143f154973607e4c932
cheandgit <104068698+cheandgit@users.noreply.github.com>
2025-10-21 17:23:03 +0500

    Create Distance.cs

---
+++ TestProject/Distance.cs
@@ -0,0 +1,12 @@
+namespace DistanceTest;
+internal class Distance
+{
+    // Расстояние от точки P(px, py) до отрезка AB с координатами A(ax, ay), B(bx, by)
+    public static double GetDistance(double px, double py, double ax, double ay, double bx, double by)
+    {
+        // ...
+        // код метода
+        // .. 
+        return 0;
+    }
+}
